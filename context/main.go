334fcc105780cd91261fa1adce031e90f3a76b43
H S <1003597101@qq.com>
2022-08-14 18:44:15 +0800

    增加上下文context

---
+++ context/main.go
@@ -0,0 +1,35 @@
+/**
+  @author: hs
+  @date: 2022/8/14
+  @note:
+
+modification history
+--------------------
+**/
+
+package main
+
+import (
+	"net/http"
+
+	"example/gee"
+)
+
+func main() {
+	r := gee.New()
+	r.GET("/", func(c *gee.Context) {
+		c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
+	})
+	r.GET("/hello", func(c *gee.Context) {
+		// expect /hello?name=hanshuai
+		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
+	})
+	r.POST("/login", func(c *gee.Context) {
+		c.JSON(http.StatusOK, gee.H{
+			"username": c.PostForm("username"),
+			"password": c.PostForm("password"),
+		})
+	})
+
+	r.Run(":9999")
+}
