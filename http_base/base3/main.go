ef2d06ccc5f1b80b9ad028c8727cda5c42d9f27e
H S <1003597101@qq.com>
2022-08-14 16:55:30 +0800

    简单Web框架demo

---
+++ http_base/base3/main.go
@@ -0,0 +1,32 @@
+/**
+  @author: hs
+  @date: 2022/8/14
+  @note:
+
+modification history
+--------------------
+**/
+
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"example/base3/gee"
+)
+
+func main() {
+	r := gee.New()
+	r.GET("/", func(w http.ResponseWriter, req *http.Request) {
+		fmt.Fprintf(w, "URL.Path = %q\n", req.URL.Path)
+	})
+
+	r.GET("/hello", func(w http.ResponseWriter, req *http.Request) {
+		for k, v := range req.Header {
+			fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
+		}
+	})
+
+	r.Run(":9999")
+}
